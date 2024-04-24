0eb824bc658ca7e7448198e5233ed9b1ea53b451
Chidindu Emmanuel <chidinduemma123@gmail.com>
2024-04-24 18:06:58 +0100

    initial commits

---
+++ src/pages/www/index.jsx
@@ -0,0 +1,21 @@
+import FooterComponent from "./footer";
+import HeaderComponent from "./header";
+
+export default function IndexPage() {
+    
+    return (
+        <main>
+            <section id="header">
+                <HeaderComponent />
+            </section>
+                
+            <section id="body-context">
+            
+            </section>
+                
+            <section id="footer">
+             <FooterComponent />
+            </section>
+        </main>
+    );
+}
---
+++ src/utils/otp.jsx
@@ -0,0 +1,3 @@
+export default function GenerateOTP(){
+  return 0o0;
+}
